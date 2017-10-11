69211194256fe8bf3e522c7004f8e4be76fff3d6
MartinLei <martin.lei@htwg-konstanz.de>
2017-10-11 19:13:55 +0200

    outline tui

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,13 +2,21 @@ package controllers
 
 import javax.inject._
 
+import minesweeper.Minesweeper
+import minesweeper.aview.tui.TextUI
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val tui: TextUI = Minesweeper.getTuiInstance
 
   def index = Action {
     Ok(views.html.index("Welcome to Minesweeper"))
   }
 
+  def minesweeper(command: String) = Action {
+    tui.processLine(command)
+    val tuiAsString: String = tui.getTUIAsString
+    Ok(views.html.minesweeper(tuiAsString))
+  }
 }
