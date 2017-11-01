b13ed62b3d9ef77813d36d1400d81f49dbd2d040
Mathias Lengler <mathmatze@googlemail.com>
2017-11-01 22:25:48 +0100

    add history page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,15 +8,19 @@ import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  val tui: TextUI = Minesweeper.getTuiInstance
+  val tuiInstance: TextUI = Minesweeper.getTuiInstance
 
   def index = Action {
     Ok(views.html.index("Welcome to Minesweeper"))
   }
 
-  def minesweeper(command: String) = Action {
-    tui.processLine(command)
-    val tuiAsString: String = tui.getTUIAsString
-    Ok(views.html.minesweeper(tuiAsString))
+  def tui(command: String) = Action {
+    tuiInstance.processLine(command)
+    val tuiAsString: String = tuiInstance.getTUIAsString
+    Ok(views.html.tui(tuiAsString))
+  }
+
+  def history = Action {
+    Ok(views.html.history())
   }
 }
