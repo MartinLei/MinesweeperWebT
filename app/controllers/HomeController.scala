cb70e880efc900ec87b3e7c8d5f097b0e967371d
Mathias Lengler <mathmatze@googlemail.com>
2017-11-16 08:01:10 +0100

    add basic grid

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,34 @@
 package controllers
 
+import collection.JavaConverters._
 import javax.inject._
 
 import minesweeper.Minesweeper
 import minesweeper.aview.tui.TextUI
+import minesweeper.controller.impl.ControllerWrapper
+import minesweeper.model.ICell
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val tuiInstance: TextUI = Minesweeper.getGlobalInjector.getInstance(classOf[TextUI])
+  val gameController: ControllerWrapper = Minesweeper.getGlobalInjector.getInstance(classOf[ControllerWrapper])
 
   def index = Action {
     Ok(views.html.index("Welcome to Minesweeper"))
   }
 
-  def tui(command: String) = Action {
+  private def getCells: List[ICell] = {
+    val cells: java.util.List[ICell] = gameController.getGrid.getCells
+
+    cells.asScala.toList
+  }
+
+  def minesweeper(command: String) = Action {
     tuiInstance.processLine(command)
+
     val tuiAsString: String = tuiInstance.getTUIAsString
-    Ok(views.html.tui(tuiAsString))
+    Ok(views.html.minesweeperView(gameController.getGrid, tuiAsString))
   }
 
   def history = Action {
