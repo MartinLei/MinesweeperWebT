8fc6f0645326819c1bbcb725c25962491d22ce88
Mathias Lengler <mathmatze@googlemail.com>
2017-11-17 22:46:54 +0100

    more css magic and add simple websocket client

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,25 @@
-if (window.console) {
-    console.log("Welcome to your Play application's JavaScript!");
-}
+console.log("Welcome to your Play application's JavaScript!");
+
+var webSocket = new WebSocket("ws://localhost:9000/ws");
+
+webSocket.onopen = function (event) {
+    console.log(event)
+};
+
+webSocket.onmessage = function (evt) {
+    console.log("onmessage: " + evt.data)
+
+};
+
+webSocket.onopen = function (event) {
+    console.log(event);
+
+    webSocket.send("{\n" +
+        "  \"action\": \"openCell\",\n" +
+        "  \"position\": {\n" +
+        "    \"row\": 1,\n" +
+        "    \"col\": 1\n" +
+        "  }\n" +
+        "}");
+
+};
