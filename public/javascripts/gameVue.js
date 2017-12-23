c0e10d0f7c37558e908be9b97f8f5b5ac96bb311
MartinLei <malei@htwg-konstanz.de>
2017-12-23 15:39:27 +0100

    add new game button

--- public/javascripts/gameVue.js
+++ public/javascripts/gameVue.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
     var minesweeperGame = new Vue({
         el: '#minesweeper-game'
+        ,
+        data: {
+            name: 'Vue.js'
+        }
     })
 
 });
 
-Vue.component('minesweeper-field', {
+Vue.component('minesweeper-controls', {
     template: `
         <div class="gamecontainer">
            <p>TEST COMPONENT</p>
+           <button v-on:click="newGame">New Game</button>
         </div>
-    `
+    `,
+    methods: {
+        newGame: function () {
+            window.gameControls.newGame()
+        }
+    }
 });
