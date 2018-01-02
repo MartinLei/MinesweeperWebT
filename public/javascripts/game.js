bd43e842e42f62942b6a3fec67836009a92e8251
MartinLei <malei@htwg-konstanz.de>
2018-01-02 17:29:22 +0100

    clean up

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -83,46 +83,13 @@ class GridController {
 
         this.wsController.send({action: "join"});
 
-        this.gridRoot = $(".grid").first();
-        this.gridRoot.contextmenu(() => false);
-
         this.statusRoot = $(".game-status").first();
 
         let vueInstance;
     }
 
-    static getCellContentElement(cell) {
-        if (cell.isRevealed) {
-            if (cell.hasMine) {
-                return $("<img/>", {
-                    "class": "grid__content grid__mine",
-                    "src": "/assets/images/mine.png"
-                });
-            } else {
-                let classString = `grid__content grid__number grid__number--${cell.surroundingMines}`;
-                let numberString = cell.surroundingMines > 0 && cell.surroundingMines;
-
-                return $("<div/>", {
-                    "class": classString
-                }).append(numberString);
-            }
-        } else {
-            if (cell.isFlagged) {
-                return $("<img/>", {
-                    "class": "grid__content grid__flag",
-                    "src": "/assets/images/flag.svg"
-                });
-            } else {
-                return $("<div/>", {
-                    "class": "grid__content grid__empty"
-                });
-            }
-        }
-    }
 
     onMessage(minesweeperEvent) {
-        this.setGridCss(minesweeperEvent.settings);
-        this.updateGrid(minesweeperEvent.grid);
         this.updateStatus(minesweeperEvent.gameState);
 
         this.updateVueGrid(minesweeperEvent.grid);
@@ -136,13 +103,6 @@ class GridController {
         } else {
             console.log("ERROR: vue not set");
         }
-    }
-
-    setGridCss(settings) {
-        this.gridRoot.css({
-            "--gridWidth": settings.width.toString(),
-            "--gridHeight": settings.height.toString(),
-        });
     }
 
     updateStatus(gameState) {
@@ -166,65 +126,6 @@ class GridController {
             )
         } else {
             this.statusRoot.empty()
-        }
-    }
-
-    updateGrid(grid) {
-        this.gridRoot.empty();
-
-        grid.forEach(row => {
-            row.forEach(cell => {
-                this.gridRoot.append(this.getCellElement(cell))
-            })
-        })
-    };
-
-    getCellElement(cell) {
-        let cellClassString =
-            `grid__cell 
-             ${cell.isFlagged ? "grid__cell--flagged" : ""}
-             ${cell.isRevealed ? "grid__cell--revealed" : ""}
-             ${!cell.isRevealed ? "grid__cell--closed" : ""}
-             ${cell.isRevealed && cell.hasMine ? "grid__cell--mine" : ""}`;
-
-        let cellElement = $("<a/>", {
-            "class": cellClassString
-        });
-
-        cellElement.append(GridController.getCellContentElement(cell));
-
-        cellElement.mousedown(e => this.onCellClick(e, cell));
-
-        return cellElement;
-    }
-
-    onCellClick(e, cell) {
-        console.log("onCellClick");
-        console.log(e);
-
-        e.preventDefault();
-
-        if (e.which === 1) {
-            // Left Click
-
-            this.wsController.send({
-                action: "openCell",
-                position: cell.position
-            });
-        } else if (e.which === 2) {
-            // Middle Click
-
-            this.wsController.send({
-                action: "openAround",
-                position: cell.position
-            });
-        } else if (e.which === 3) {
-            // Right Click
-
-            this.wsController.send({
-                action: "toggleFlag",
-                position: cell.position
-            });
         }
     }
 
