a317935fb8875ea927a9df118ddec2003f9ab11b
Mathias Lengler <mathmatze@googlemail.com>
2018-01-06 05:14:25 +0100

    render signIn page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,13 +2,25 @@ package controllers
 
 import javax.inject._
 
+import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
 import minesweeper.Minesweeper
 import minesweeper.aview.tui.TextUI
 import minesweeper.controller.impl.ControllerWrapper
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()
+(
+  cc: ControllerComponents,
+  socialProviderRegistry: SocialProviderRegistry
+)(
+  implicit
+  assets: AssetsFinder,
+  ex: ExecutionContext
+) extends AbstractController(cc) with I18nSupport {
   val tuiInstance: TextUI = Minesweeper.getGlobalInjector.getInstance(classOf[TextUI])
   val gameController: ControllerWrapper = Minesweeper.getGlobalInjector.getInstance(classOf[ControllerWrapper])
 
@@ -29,6 +41,11 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   def history = Action {
     Ok(views.html.history())
+  }
+
+  def signIn = Action {
+    implicit request: Request[AnyContent] =>
+      Ok(views.html.signIn(socialProviderRegistry))
   }
 
   def polymerGame = Action{
